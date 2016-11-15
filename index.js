a2f1ade7f612705483840da8e8f0a456f755b90b
varunpal <varun.pal@myntra.com>
2016-11-15 16:55:37 +0530

    Check if hasOwnProperty exists.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var at = function (obj, keys) {
   if (obj && (typeof keys === 'string')) {
     keyArray = keys.split('.');
     for (index = 0; index < keyArray.length; index ++) {
-      if (resolved && resolved.hasOwnProperty(keyArray[index])) {
+      if (resolved && (typeof resolved.hasOwnProperty === 'function') && resolved.hasOwnProperty(keyArray[index])) {
         resolved = resolved[keyArray[index]]
       } else {
         resolved = undefined;
